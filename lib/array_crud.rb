7e961e912edd8f328b1f2c2a826058ce79e7be8b
Heather Kirk <hskirk@gmail.com>
2019-01-07 02:22:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array("wow", "i", "really", "learning", element)
-  element("Puppy")
+  element("arrays!")
 end
 
 def add_element_to_start_of_array(array, unshift)
