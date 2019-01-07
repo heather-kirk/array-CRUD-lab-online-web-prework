b7845e051c7552c050463e1284aa61a2d8ae2d7a
Heather Kirk <hskirk@gmail.com>
2019-01-07 02:18:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,21 @@
 def create_an_empty_array
-  
+[]
 end
 
 def create_an_array
-  
+  ["Vivi", "Piccolo", "Nanook", "Westley"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  add_element_to_end_of_array("Puppy")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  pets.unshift ("Kitty")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  pets.pop ("Puppy")
 end
 
 def remove_element_from_start_of_array(array)
