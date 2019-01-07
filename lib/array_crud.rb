bf75f2920bbe44afc868c2995568e6681b7827e0
Heather Kirk <hskirk@gmail.com>
2019-01-07 02:30:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,12 +17,12 @@ def add_element_to_start_of_array(array, unshift)
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["Vivi", "Piccolo", "Nanook", "Westley"]
+  array = ["Vivi", "Piccolo", "Nanook", "arrays!"]
   array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  array = ["Vivi", "Piccolo", "Nanook", "Westley"]
+  array = ["wow", "Piccolo", "Nanook", "Westley"]
   array.shift
 end
 
