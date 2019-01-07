cb194d00f3d20c4528aa1dc838e6fc6ff4923537
Heather Kirk <hskirk@gmail.com>
2019-01-07 02:28:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["Vivi", "Piccolo", "Nanook", "Westley"]
-  array.pop ("Puppy")
+  array.pop ("arrays!")
 end
 
 def remove_element_from_start_of_array(array)
