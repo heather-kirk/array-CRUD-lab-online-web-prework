3f46e0c4a1b6a6f77b8b662377d789792789ceff
Heather Kirk <hskirk@gmail.com>
2019-01-07 02:24:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,7 +6,7 @@ def create_an_array
   ["Vivi", "Piccolo", "Nanook", "Westley"]
 end
 
-def add_element_to_end_of_array(array, element)
+def add_element_to_end_of_array(array, element = "arrays!")
   element("arrays!")
 end
 
