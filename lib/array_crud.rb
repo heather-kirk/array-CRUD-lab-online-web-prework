023f644f64479933ebb4756090c1382e50efd6fa
Heather Kirk <hskirk@gmail.com>
2019-01-07 02:27:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, unshift)
-  unshift ("Kitty")
+  array = ["Vivi", "Piccolo", "Nanook", "Westley"]
+  array.unshift ("Kitty")
 end
 
 def remove_element_from_end_of_array(array)
